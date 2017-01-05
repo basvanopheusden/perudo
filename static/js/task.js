1ebadc8e22a89f17944a0e5ceea2566f1913a03c
basvanopheusden <svo213@nyu.edu>
2017-01-05 15:36:42 -0500

    added transparency

--- static/js/task.js
+++ static/js/task.js
@@ -4,6 +4,8 @@ var ndice_self;
 var ndice_opp;
 var trials_completed;
 var data;
+var n_blocks=2;
+var trials_per_block=5;
 
 var task = function(){
 	trials_completed=0;
@@ -35,18 +37,16 @@ function start_block(){
 	do_trial();
 }
 
-function respond(resp,correct_resp,r,d_opp){
+function respond(resp,correct_resp,r,d_opp,d_target){
 	$('#yesbutton').prop("disabled",true);
 	$('#nobutton').prop("disabled",true);
 	$("#" + (resp==true ? "yes" : "no") + "button").css("border","3px solid #222222");
-	showdice_opp(d_opp);
-	if(resp==correct_resp){
-		$("#feedback").text("Correct: " + r.toString() + " point" + (r>1 ? "s" : ""));
-	}
-	else {
-		$("#feedback").text("Incorrect");
-	}
+	showdice_opp(d_opp,d_target);
+	$("#feedback p").first().text(correct_resp?"True":"False");
+	$("#feedback p").last().text(correct_resp==resp?"$" + r.toString():"");
 	$("#feedback").show();
+	$(".die").css("opacity",0.5);
+	$(".correct_die").css("opacity",1);	
 	add_resp_to_data(resp,correct_resp);
 	$('#nextbutton').show();
 	$('#nextbutton').prop("disabled", false);
@@ -74,69 +74,77 @@ function save(filename){
 }
 
 function click_next(){
-	$('#yesbutton').prop("disabled",false);
-	$('#nobutton').prop("disabled",false);
-	$("#yesbutton").css("border","");
-	$("#nobutton").css("border","");
 	$('#nextbutton').prop("disabled", true);
 	$('#nextbutton').hide();
 	$('#feedback').hide();
 	hidedice_opp();
 	trials_completed++;
-	if(trials_completed==10){
+	$(".die").removeClass("correct_die").css("opacity",1);
+	if(trials_completed==n_blocks*trials_per_block){
 		save("perudo_responses.txt");
 	}
-	else if(trials_completed%5==0){
-		start_block();
+	else {
+		$('#yesbutton').prop("disabled",false);
+		$('#nobutton').prop("disabled",false);
+		$("#yesbutton").css("border","");
+		$("#nobutton").css("border","");
+		if(trials_completed%trials_per_block==0){
+			start_block();
+		}
+		else{
+			do_trial();
+		}
 	}
-	else{
-		do_trial();
-	}
-	
 }
 
 function do_trial(){
 	var d_self=[];
 	var d_opp=[];
 	var n=0;
-	var d_statement=Math.floor(ncats*Math.random());
+	var d_target=Math.floor(ncats*Math.random());
 	var r_yes=Math.ceil(5*Math.random());
 	var r_no=Math.ceil(5*Math.random());
 	var n_target;
 	for(i=0;i<ndice_self;i++){
 		d_self[i]=Math.floor(ncats*Math.random());
-		if(d_statement==d_self[i]){n++};
+		if(d_target==d_self[i]){n++};
 	}
 	n_target=n+Math.ceil(ndice_opp*Math.random());
 	for(i=0;i<ndice_opp;i++){
 		d_opp[i]=Math.floor(ncats*Math.random());
-		if(d_statement==d_opp[i]){n++};
+		if(d_target==d_opp[i]){n++};
 	}
 	set_rewards(r_yes,r_no);
-	showdice_self(d_self);
-	showstatement(n_target,d_statement);
+	showdice_self(d_self,d_target);
+	showstatement(n_target,d_target);
 	correct_resp=(n>=n_target);
 	r=(correct_resp==true ? r_yes : r_no);
-	add_stim_to_data(n_target, d_statement, d_self, d_opp, r_yes, r_no);
-	$('#yesbutton').off("click").click(function(){respond(true,correct_resp,r,d_opp);});	
-	$('#nobutton').off("click").click(function(){respond(false,correct_resp,r,d_opp);});	
+	add_stim_to_data(n_target, d_target, d_self, d_opp, r_yes, r_no);
+	$('#yesbutton').off("click").click(function(){respond(true,correct_resp,r,d_opp,d_target);});	
+	$('#nobutton').off("click").click(function(){respond(false,correct_resp,r,d_opp,d_target);});	
 }
 
 function set_rewards(r_yes,r_no){
-	$('#yesbutton').text("Yes: " + r_yes.toString() + " point" + (r_yes>1? "s": ""))
-	$('#nobutton').text("No: " + r_no.toString() + " point" + (r_no>1? "s": ""))	
+	$('#yesbutton p').last().text("$" + r_yes.toString());
+	$('#nobutton p').last().text("$" + r_no.toString());
 }
 
-function showdice_self(l){
+function showdice_self(l,t){
 	for(i=0;i<ndice_self;i++){
 		$('#'+"die_self"+i.toString()).css("background-image","url('media/" + images[l[i]] + "')");
+		if(l[i]==t){
+			$('#'+"die_self"+i.toString()).addClass("correct_die");
+		}
 	}
 }
 
-function showdice_opp(l){
+function showdice_opp(l,t){
 	$(".die p").hide();
 	for(i=0;i<ndice_opp;i++){
 		$('#'+"die_opp"+i.toString()).css("background-image","url('media/" + images[l[i]] + "')");
+		if(l[i]==t){
+			$('#'+"die_opp"+i.toString()).addClass("correct_die");
+		}
 	}
 }
 
